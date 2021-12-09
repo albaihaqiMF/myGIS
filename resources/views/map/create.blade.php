6530463dba522c3d4a0e85d7a92787a6caf5453c
albaihaqiMF <241099+albaihaqiMF@users.noreply.github.com>
2021-12-09 22:10:14 +0700

    Create Data 091221

--- resources/views/map/create.blade.php
+++ resources/views/map/create.blade.php
@@ -4,31 +4,53 @@
             <form action="{{ route('map.store') }}" enctype="multipart/form-data" method="POST" class="box px-4 py-6">
                 @csrf
                 <div class="mb-3">
-                    <label for="name" class="form-label">Name</label>
+                    <label for="name" class="form-label w-full flex flex-col sm:flex-row">Name
+                        @error('name')
+                        <span class="sm:ml-auto mt-1 sm:mt-0 text-xs text-gray-600">{{ $message }}</span>
+                        @enderror
+                    </label>
                     <input id="name" name="name" type="text" class="form-control w-full" placeholder="Name"
                         value="{{ old('name') }}">
                 </div>
                 <div class="mb-3">
-                    <label for="gambar_taksasi" class="form-label">Taksasi</label>
+                    <label for="gambar_taksasi" class="form-label w-full flex flex-col sm:flex-row">Taksasi
+                        @error('gambar_taksasi')
+                        <span class="sm:ml-auto mt-1 sm:mt-0 text-xs text-gray-600">{{ $message }}</span>
+                        @enderror
+                    </label>
                     <input id="gambar_taksasi" name="gambar_taksasi" type="file" accept="image/*"
                         class="form-control w-full" placeholder="Taksasi">
                 </div>
                 <div class="mb-3">
-                    <label for="gambar_ndvi" class="form-label">NDVI</label>
+                    <label for="gambar_ndvi" class="form-label w-full flex flex-col sm:flex-row">NDVI
+                        @error('gambar_ndvi')
+                        <span class="sm:ml-auto mt-1 sm:mt-0 text-xs text-gray-600">{{ $message }}</span>
+                        @enderror
+                    </label>
                     <input id="gambar_ndvi" name="gambar_ndvi" type="file" accept="image/*" class="form-control w-full"
                         placeholder="NDVI">
                 </div>
                 <div class="mb-3 grid grid-cols-2 gap-4">
                     <div class="col-span-2 md:col-span-1">
                         <div>
-                            <label for="sw_latitude" class="form-label">Southwest Latitude</label>
+                            <label for="sw_latitude" class="form-label w-full flex flex-col sm:flex-row">Southwest
+                                Latitude
+                                @error('sw_latitude')
+                                <span class="sm:ml-auto mt-1 sm:mt-0 text-xs text-gray-600">{{ $message
+                                    }}</span></label>
+                            @enderror
                             <input id="sw_latitude" name="sw_latitude" type="text" class="form-control w-full"
                                 placeholder="Southwest Latitude" value="{{ old('sw_latitude') }}">
                         </div>
                     </div>
                     <div class="col-span-2 md:col-span-1">
                         <div>
-                            <label for="sw_longitude" class="form-label">Southwest Longitude</label>
+                            <label for="sw_longitude" class="form-label w-full flex flex-col sm:flex-row">Southwest
+                                Longitude
+                                @error('sw_longitude')
+                                <span class="sm:ml-auto mt-1 sm:mt-0 text-xs text-gray-600">{{ $message
+                                    }}</span></label>
+                            @enderror
                             <input id="sw_longitude" name="sw_longitude" type="text" class="form-control w-full"
                                 placeholder="Southwest Longitude" value="{{ old('sw_longitude') }}">
                         </div>
@@ -37,14 +59,24 @@
                 <div class="mb-3 grid grid-cols-2 gap-4">
                     <div class="col-span-2 md:col-span-1">
                         <div>
-                            <label for="ne_latitude" class="form-label">Northeast Latitude</label>
+                            <label for="ne_latitude" class="form-label w-full flex flex-col sm:flex-row">Northeast
+                                Latitude
+                                @error('ne_latitude')
+                                <span class="sm:ml-auto mt-1 sm:mt-0 text-xs text-gray-600">{{ $message
+                                    }}</span></label>
+                            @enderror
                             <input id="ne_latitude" name="ne_latitude" type="text" class="form-control w-full"
                                 placeholder="Northeast Latitude" value="{{ old('ne_latitude') }}">
                         </div>
                     </div>
                     <div class="col-span-2 md:col-span-1">
                         <div>
-                            <label for="ne_longitude" class="form-label">Northeast Longitude</label>
+                            <label for="ne_longitude" class="form-label w-full flex flex-col sm:flex-row">Northeast
+                                Longitude
+                                @error('ne_longitude')
+                                <span class="sm:ml-auto mt-1 sm:mt-0 text-xs text-gray-600">{{ $message
+                                    }}</span></label>
+                            @enderror
                             <input id="ne_longitude" name="ne_longitude" type="text" class="form-control w-full"
                                 placeholder="Northeast Longitude" value="{{ old('ne_longitude') }}">
                         </div>
@@ -60,5 +92,4 @@
             </form>
         </div>
     </div>
-
 </x-app-layout>
