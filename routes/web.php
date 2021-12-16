cb4a97dc4459c9626525b6a8fc457cbffa9c7fdd
albaihaqiMF <241099+albaihaqiMF@users.noreply.github.com>
2021-12-16 15:41:59 +0700

    bg login

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [Controller::class, 'dashboard']);
+Route::view('/', 'auth.login');
 
 Route::middleware('auth')->group(function () {
     Route::get('dashboard', [Controller::class, 'dashboard'])->name('dashboard');
