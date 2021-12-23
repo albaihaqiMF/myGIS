35dde6c1972e74781ee9c9fb4114223b623c3bd7
albaihaqiMF <241099+albaihaqiMF@users.noreply.github.com>
2021-12-23 11:46:59 +0700

    new style tailwindcss v3

--- resources/views/map/create.blade.php
+++ resources/views/map/create.blade.php
@@ -1,89 +1,89 @@
 <x-app-layout title="Input Map Data">
-    <div class="grid grid-cols-12 w-full pt-4 gap-4">
-        <div class="col-span-full 2xl:col-span-8 gap-4 intro-y">
-            <form action="{{ route('map.store') }}" enctype="multipart/form-data" method="POST" class="box px-4 py-6">
+    <div class="grid w-full grid-cols-12 gap-4 pt-4">
+        <div class="gap-4 col-span-full 2xl:col-span-8 intro-y">
+            <form action="{{ route('map.store') }}" enctype="multipart/form-data" method="POST" class="px-4 py-6 box">
                 @csrf
                 <div class="mb-3">
-                    <label for="name" class="form-label w-full flex flex-col sm:flex-row">Name
+                    <label for="name" class="flex flex-col w-full form-label sm:flex-row">Name
                         @error('name')
-                        <span class="sm:ml-auto mt-1 sm:mt-0 text-xs text-gray-600">{{ $message }}</span>
+                        <span class="mt-1 text-xs text-gray-600 sm:ml-auto sm:mt-0">{{ $message }}</span>
                         @enderror
                     </label>
-                    <input id="name" name="name" type="text" class="form-control w-full" placeholder="Name"
+                    <input id="name" name="name" type="text" class="w-full form-control" placeholder="Name"
                         value="{{ old('name') }}">
                 </div>
                 <div class="mb-3">
-                    <label for="gambar_taksasi" class="form-label w-full flex flex-col sm:flex-row">Taksasi
+                    <label for="gambar_taksasi" class="flex flex-col w-full form-label sm:flex-row">Taksasi
                         @error('gambar_taksasi')
-                        <span class="sm:ml-auto mt-1 sm:mt-0 text-xs text-gray-600">{{ $message }}</span>
+                        <span class="mt-1 text-xs text-gray-600 sm:ml-auto sm:mt-0">{{ $message }}</span>
                         @enderror
                     </label>
                     <input id="gambar_taksasi" name="gambar_taksasi" type="file" accept="image/*"
-                        class="form-control w-full" placeholder="Taksasi">
+                        class="w-full transition cursor-pointer form-control file:bg-indigo-50 file:hover:bg-indigo-100 file:rounded-full file:px-3 file:py-1 file:border-none file:text-indigo-800 file:duration-300" placeholder="Taksasi">
                 </div>
                 <div class="mb-3">
-                    <label for="gambar_ndvi" class="form-label w-full flex flex-col sm:flex-row">NDVI
+                    <label for="gambar_ndvi" class="flex flex-col w-full form-label sm:flex-row">NDVI
                         @error('gambar_ndvi')
-                        <span class="sm:ml-auto mt-1 sm:mt-0 text-xs text-gray-600">{{ $message }}</span>
+                        <span class="mt-1 text-xs text-gray-600 sm:ml-auto sm:mt-0">{{ $message }}</span>
                         @enderror
                     </label>
-                    <input id="gambar_ndvi" name="gambar_ndvi" type="file" accept="image/*" class="form-control w-full"
+                    <input id="gambar_ndvi" name="gambar_ndvi" type="file" accept="image/*" class="w-full transition cursor-pointer form-control file:bg-indigo-50 file:hover:bg-indigo-100 file:rounded-full file:px-3 file:py-1 file:border-none file:text-indigo-800 file:duration-300"
                         placeholder="NDVI">
                 </div>
-                <div class="mb-3 grid grid-cols-2 gap-4">
+                <div class="grid grid-cols-2 gap-4 mb-3">
                     <div class="col-span-2 md:col-span-1">
-                        <label for="sw_latitude" class="form-label w-full flex flex-col sm:flex-row">Southwest
+                        <label for="sw_latitude" class="flex flex-col w-full form-label sm:flex-row">Southwest
                             Latitude
                             @error('sw_latitude')
-                            <span class="sm:ml-auto mt-1 sm:mt-0 text-xs text-gray-600">{{ $message
+                            <span class="mt-1 text-xs text-gray-600 sm:ml-auto sm:mt-0">{{ $message
                                 }}</span>
                             @enderror
                         </label>
-                        <input id="sw_latitude" name="sw_latitude" type="text" class="form-control w-full"
+                        <input id="sw_latitude" name="sw_latitude" type="text" class="w-full form-control"
                             placeholder="Southwest Latitude" value="{{ old('sw_latitude') }}">
                     </div>
                     <div class="col-span-2 md:col-span-1">
-                        <label for="sw_longitude" class="form-label w-full flex flex-col sm:flex-row">Southwest
+                        <label for="sw_longitude" class="flex flex-col w-full form-label sm:flex-row">Southwest
                             Longitude
                             @error('sw_longitude')
-                            <span class="sm:ml-auto mt-1 sm:mt-0 text-xs text-gray-600">{{ $message
+                            <span class="mt-1 text-xs text-gray-600 sm:ml-auto sm:mt-0">{{ $message
                                 }}</span>
                             @enderror
                         </label>
-                        <input id="sw_longitude" name="sw_longitude" type="text" class="form-control w-full"
+                        <input id="sw_longitude" name="sw_longitude" type="text" class="w-full form-control"
                             placeholder="Southwest Longitude" value="{{ old('sw_longitude') }}">
                     </div>
                 </div>
-                <div class="mb-3 grid grid-cols-2 gap-4">
+                <div class="grid grid-cols-2 gap-4 mb-3">
                     <div class="col-span-2 md:col-span-1">
-                        <label for="ne_latitude" class="form-label w-full flex flex-col sm:flex-row">Northeast
+                        <label for="ne_latitude" class="flex flex-col w-full form-label sm:flex-row">Northeast
                             Latitude
                             @error('ne_latitude')
-                            <span class="sm:ml-auto mt-1 sm:mt-0 text-xs text-gray-600">{{ $message
+                            <span class="mt-1 text-xs text-gray-600 sm:ml-auto sm:mt-0">{{ $message
                                 }}</span>
                             @enderror
                         </label>
-                        <input id="ne_latitude" name="ne_latitude" type="text" class="form-control w-full"
+                        <input id="ne_latitude" name="ne_latitude" type="text" class="w-full form-control"
                             placeholder="Northeast Latitude" value="{{ old('ne_latitude') }}">
                     </div>
                     <div class="col-span-2 md:col-span-1">
-                        <label for="ne_longitude" class="form-label w-full flex flex-col sm:flex-row">Northeast
+                        <label for="ne_longitude" class="flex flex-col w-full form-label sm:flex-row">Northeast
                             Longitude
                             @error('ne_longitude')
-                            <span class="sm:ml-auto mt-1 sm:mt-0 text-xs text-gray-600">{{ $message
+                            <span class="mt-1 text-xs text-gray-600 sm:ml-auto sm:mt-0">{{ $message
                                 }}</span>
                             @enderror
                         </label>
-                        <input id="ne_longitude" name="ne_longitude" type="text" class="form-control w-full"
+                        <input id="ne_longitude" name="ne_longitude" type="text" class="w-full form-control"
                             placeholder="Northeast Longitude" value="{{ old('ne_longitude') }}">
                     </div>
                 </div>
-                <div class="flex flex-col md:flex-row justify-end pt-3 gap-4">
-                    <button class="btn bg-primary-1 w-full md:w-24 text-white font-semibold">
+                <div class="flex flex-col justify-end gap-4 pt-3 md:flex-row">
+                    <button class="w-full font-semibold text-white btn bg-primary-1 md:w-24">
                         SAVE
                     </button>
                     <a href="{{ route('map.list') }}"
-                        class="btn btn-outline-secondary w-full md:w-24 font-semibold">CANCEL</a>
+                        class="w-full font-semibold btn btn-outline-secondary md:w-24">CANCEL</a>
                 </div>
             </form>
         </div>
