09cb1ca8d0ed140c74e7d0507a6ab9cee30e7d94
albaihaqiMF <241099+albaihaqiMF@users.noreply.github.com>
2022-02-27 15:57:09 +0700

    menu fixed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -179,9 +179,6 @@ License: You must have a valid license purchased only from themeforest(the above
                                 @endif
                                 <a href=""
                                     class="flex items-center block p-2 transition duration-300 ease-in-out hover:bg-gray-200 dark:hover:bg-dark-3 rounded-md">
-                                    <i data-feather="lock" class="w-4 h-4 mr-2"></i> Reset Password </a>
-                                <a href=""
-                                    class="flex items-center block p-2 transition duration-300 ease-in-out hover:bg-gray-200 dark:hover:bg-dark-3 rounded-md">
                                     <i data-feather="help-circle" class="w-4 h-4 mr-2"></i> Help </a>
                             </div>
                             <div class="p-2 border-t border-black border-opacity-5 dark:border-dark-3 w-full">
